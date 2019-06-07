f3114ffb3d4813edbf4a3b33190755e27080d743
klforthwind <kl.forthwind@gmail.com>
2019-06-07 01:41:29 +0000

    more nodejs project

--- www/blog/index.js
+++ www/blog/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const markdown = require("markdown");
 const app = express();
 
 app.get('/blog/', (req, res) => {
     res.send("Welcome to Express!");
+});
+
+app.get('/blog/:post', (req, res) => {
+    res.send(`This is the ${req.params.name} Post!`);
 });
 
 app.listen(3000);
